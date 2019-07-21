88012d43d8f43ecf2495057da88cb35dd29fa33e
Cheekyfunkymonkey <shigechu@gmail.com>
2019-07-22 08:39:37 +1200

    Create controllers/comments.js and the test for it

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const express = require("express");
-const commentsRouter = require("./routers/comments");
-const app = express();
+const app = require("./app");
 const PORT = 8080;
-
-app.use("/api/comments", commentsRouter);
 
 app.listen(PORT, function() {
   console.log(`Comments app is listening on port ${PORT}.`);
